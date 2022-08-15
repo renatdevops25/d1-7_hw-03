6fe839c80d6e78369187b710067582d742aca84d
renatdevops25 <renatdevops25@gmail.com>
2022-08-15 21:35:47 +0000

    Added files

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "token" {
+  description = "token"
+  type        = string
+  default     = "XXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+}
+
+variable "cloud_id" {
+  description = "Cloud ID"
+  type        = string
+  default     = "XXXXXXXXXXXXXXXXXXXXXXXXXXX"
+}
+
+variable "folder_id" {
+  description = "Folder ID"
+  type        = string
+  default     = "XXXXXXXXXXXXXXXXXXXXXXXXXXX"
+}
